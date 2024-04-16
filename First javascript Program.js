2e2ea96d944e3412745fd4e453abfd1ae3a34d67
Arbaazahmed06 <arbaazahmed0607@gmail.com>
2024-04-16 13:49:57 +0530

    Firstcode

---
+++ First javascript Program.js
@@ -0,0 +1,6 @@
+// Write the program to greet a person given there First name and last name.
+
+var firstname = 'John';
+var lastname = 'Ahmed';
+
+console.log('Hello, ' + firstname + ' ' + lastname + '!');
