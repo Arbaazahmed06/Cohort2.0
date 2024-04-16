1195a1ccc0b1ef1534232a72fd408c48959b6d0c
Arbaazahmed06 <arbaazahmed0607@gmail.com>
2024-04-16 14:05:57 +0530

    Secondcode

---
+++ SecondJavaScriptProgram.js
@@ -0,0 +1,11 @@
+// write a program to greet a person based on their gender (if else)
+
+var PersonName = "Arbaaz";
+var Gender = "123";
+
+if (Gender == "Male") {
+    console.log("HI " + PersonName);
+} else if (Gender == "Female") {
+    console.log("Hello " + PersonName);
+} else
+    console.log("HEY " + PersonName);
